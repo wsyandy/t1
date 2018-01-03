258649b892966f19d441b0573d79bc2734ecc146
康广庆 <kanggq@365yf.com>
2018-01-03 16:13:50 +0800

    key

---
+++ app/controllers/api/ProductChannelsController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 18/1/3
+ * Time: 下午3:15
+ */
+
+namespace api;
+
+
+class ProductChannelsController extends BaseController
+{
+    function aboutAction()
+    {
+        $product_channel = $this->currentProductChannel();
+        return $this->renderJSON(ERROR_CODE_SUCCESS, '', $product_channel->toAboutJson());
+    }
+
+}
