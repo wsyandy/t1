b1260739d4028534695b2041b55c446b0458ebc1
梅兴昊 <apple@appledeiMac.local>
2018-01-04 19:39:56 +0800

    bug

--- app/controllers/api/FriendsController.php
+++ app/controllers/api/FriendsController.php
@@ -18,7 +18,11 @@ class FriendsController extends BaseController
         $per_page = $this->params('per_page', 10);
         $users = $this->currentUser()->friendList($page, $per_page, $new);
 
-        return $this->renderJSON(ERROR_CODE_SUCCESS, '', $users->toJson('users', 'toRelationJson'));
+        if ($users) {
+            return $this->renderJSON(ERROR_CODE_SUCCESS, '', $users->toJson('users', 'toRelationJson'));
+        }
+
+        return $this->renderJSON(ERROR_CODE_SUCCESS, '');
     }
 
     //添加好友
