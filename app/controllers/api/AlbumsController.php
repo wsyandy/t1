1a3d696d35bb739047187980561cfe19d433a1b9
梅兴昊 <apple@appledeiMac.local>
2018-01-06 13:01:28 +0800

    log

--- app/controllers/api/AlbumsController.php
+++ app/controllers/api/AlbumsController.php
@@ -49,6 +49,7 @@ class AlbumsController extends BaseController
     function destroyAction()
     {
 
+        debug($this->params(), $this->headers());
         $album = \Albums::findFirstById($this->params('id', 0));
         if ($album) {
             $album->delete();
