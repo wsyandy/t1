cd282bb89043f6f1b2c2287f74bcef0fac3be396
毛孪娟 <maoluanjuan@maoluanjuandeMacBook-Pro.local>
2018-01-06 10:55:53 +0800

    支付通道controller

---
+++ app/controllers/admin/PaymentChannelsController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maoluanjuan
+ * Date: 06/01/2018
+ * Time: 10:50
+ */
+namespace admin;
+
+class PaymentChannelsController extends BaseController
+{
+    function indexAction()
+    {
+        $payment_channels = \PaymentChannels::find();
+        $this->view->payment_channels = $payment_channels;
+    }
+
+    function newAction()
+    {
+        $payment_channel = new \PaymentChannels();
+        $this->view->payment_channel = $payment_channel;
+    }
+
+    function createAction()
+    {
+        $payment_channel = new \PaymentChannels();
+        $this->assign($payment_channel, 'payment_channel');
+        if ($payment_channel->create()) {
+            return $this->renderJSON(ERROR_CODE_SUCCESS, '',
+                array('payment_channel' => $payment_channel->toJson()));
+        } else {
+            return $this->renderJSON(ERROR_CODE_FAIL, '');
+        }
+    }
+
+    function editAction()
+    {
+        $payment_channel = \PaymentChannels::findById($this->params('id'));
+        $this->view->payment_channel = $payment_channel;
+    }
+
+    function updateAction()
+    {
+       $payment_channel = \PaymentChannels::findById($this->params('id'));
+       $this->assign($payment_channel, 'payment_channel');
+        if ($payment_channel->create()) {
+            return $this->renderJSON(ERROR_CODE_SUCCESS, '',
+                array('payment_channel' => $payment_channel->toJson()));
+        } else {
+            return $this->renderJSON(ERROR_CODE_FAIL, '');
+        }
+    }
+}
