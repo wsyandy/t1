279b2eaff0320d756994668194cc84bf1eb3c5cf
梅兴昊 <apple@appledeiMac.local>
2018-01-09 19:18:07 +0800

    优化

--- app/controllers/api/RoomsController.php
+++ app/controllers/api/RoomsController.php
@@ -121,8 +121,8 @@ class RoomsController extends BaseController
 
 
         //如果进入其他房间时 用户身上有房间 先退出房间
-        if ($this->currentUser()->current_room && $this->currentUser()->current_room_id != $room_id) {
-            $room->exitRoom($this->currentUser());
+        if ($this->currentUser()->current_room && $this->currentUser()->current_room->id != $room_id) {
+            $this->currentUser()->current_room->exitRoom($this->currentUser());
         }
 
         $room->enterRoom($this->currentUser());
@@ -143,6 +143,7 @@ class RoomsController extends BaseController
     {
         $room_id = $this->params('id', 0);
         $room = \Rooms::findFirstById($room_id);
+
         if (!$room) {
             return $this->renderJSON(ERROR_CODE_FAIL, '参数非法');
         }
