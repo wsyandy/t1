62ed4ffe2fadc84ff3338885f1ea1e135b0644ef
梅兴昊 <apple@appledeiMac.local>
2018-01-12 16:56:40 +0800

    登录密码限制

--- app/controllers/api/RoomsController.php
+++ app/controllers/api/RoomsController.php
@@ -99,8 +99,10 @@ class RoomsController extends BaseController
             return $this->renderJSON(ERROR_CODE_FAIL, '您被禁止禁入房间,请稍后尝试');
         }
 
-        //房间加锁并且不是房主检验密码
-        if ($room->lock && $room->password != $password && $room->user_id != $this->currentUser()->id) {
+        $current_user_id = $this->currentUser()->id;
+        $current_room_id = $this->currentUser()->current_room_id;
+        //房间加锁并且不是房主且用户不在这个房间检验密码
+        if ($room->lock && $room->password != $password && $room->user_id != $current_user_id && $current_room_id != $room->id) {
             return $this->renderJSON(ERROR_CODE_FORM, '密码错误');
         }
 
