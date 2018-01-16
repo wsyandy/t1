755760b34d54c9d0a4c473c104761a105193a12d
梅兴昊 <apple@appledeiMac.local>
2018-01-16 21:31:46 +0800

    优化

--- app/models/PaymentChannels.php
+++ app/models/PaymentChannels.php
@@ -28,7 +28,7 @@ class PaymentChannels extends BaseModel
         ];
     }
 
-    function getGatewayClasses()
+    static function getGatewayClasses()
     {
         return \paygateway\Base::getGatewayNames();
     }
