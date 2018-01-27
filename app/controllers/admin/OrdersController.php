1dee8b09071d79d4ff2d5ec00858b1e5f3014e5b
杨星 <apple@appledeiMac.local>
2018-01-27 14:27:28 +0800

    订单添加搜索

--- app/controllers/admin/OrdersController.php
+++ app/controllers/admin/OrdersController.php
@@ -12,28 +12,30 @@ class OrdersController extends BaseController
 {
     function indexAction()
     {
-        $cond = array('order' => 'id desc');
+//        $cond = array('order' => 'id desc');
         $page = 1;
         $per_page = 30;
-        $cond_vars = array();
-        $cond_values = array();
-        foreach (['user_id', 'id'] as $item) {
-            if (isPresent($this->params($item))) {
-                $cond_vars[] = $item . ' = ' . ':' . $item . ':';
-                $cond_values[$item] = $this->params($item);
-            }
-        }
-        if (isPresent($cond_vars)) {
-            $conditions = implode('and', $cond_vars);
-            $cond = array(
-                'conditions' => $conditions,
-                'bind' => $cond_values,
-                'order' => 'id desc'
-            );
-        }
-        debug($cond);
+//        $cond_vars = array();
+//        $cond_values = array();
+//        foreach (['user_id', 'id'] as $item) {
+//            if (isPresent($this->params($item))) {
+//                $cond_vars[] = $item . ' = ' . ':' . $item . ':';
+//                $cond_values[$item] = $this->params($item);
+//            }
+//        }
+//        if (isPresent($cond_vars)) {
+//            $conditions = implode('and', $cond_vars);
+//            $cond = array(
+//                'conditions' => $conditions,
+//                'bind' => $cond_values,
+//                'order' => 'id desc'
+//            );
+//        }
+        $cond = $this->getConditions('order');
+        $cond['order'] = 'id desc';
         $orders = \Orders::findPagination($cond, $page, $per_page);
         $this->view->orders = $orders;
+        $this->view->product_channels = \ProductChannels::find(['order'=>'id desc']);
     }
 
     function detailAction()
