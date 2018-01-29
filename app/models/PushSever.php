53904eccfcb4d87a7614e66b7fadbff5a88ec502
梅兴昊 <apple@appledeiMac.local>
2018-01-29 22:03:10 +0800

    log

--- app/models/PushSever.php
+++ app/models/PushSever.php
@@ -155,7 +155,7 @@ class PushSever extends BaseModel
     function onOpen($server, $request)
     {
         $fd = $request->fd;
-
+        info("connection_num", $fd);
         $connect_info = $server->connection_info($fd);
         $server_port = fetch($connect_info, 'server_port');
 
@@ -179,7 +179,7 @@ class PushSever extends BaseModel
 
         $hot_cache = self::getHotWriteCache();
         $hot_cache->zincrby($this->connection_list, 1, $ip);
-        info("connection_num", $this->connection_list, $ip, $hot_cache->zscore($this->connection_list, $ip));
+        info("connection_num", $fd, $this->connection_list, $ip, $hot_cache->zscore($this->connection_list, $ip));
         if (!$user) {
             $data = ['online_token' => $online_token, 'action' => 'create_token', 'error_code' => ERROR_CODE_FAIL, 'error_reason' => '用户不存在'];
             $server->push($request->fd, json_encode($data, JSON_UNESCAPED_UNICODE));
@@ -272,7 +272,7 @@ class PushSever extends BaseModel
     {
         $connect_info = $server->connection_info($fd);
         $server_port = fetch($connect_info, 'server_port');
-
+        info("connection_num", $fd);
         if ($this->websocket_listen_server_port == $server_port) {
             info($fd, "server_to_server onClose");
             return;
@@ -298,7 +298,7 @@ class PushSever extends BaseModel
         $local_ip = self::getIntranetIp();
         if ($local_ip) {
             $hot_cache->zincrby($this->connection_list, -1, $local_ip);
-            info("connection_num", $this->connection_list, $local_ip, $hot_cache->zscore($this->connection_list, $local_ip));
+            info("connection_num", $fd, $this->connection_list, $local_ip, $hot_cache->zscore($this->connection_list, $local_ip));
         }
 
         if ($user) {
