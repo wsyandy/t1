8eb42a1bc5e7185834c72d3bc9d35d37c564a23b
梅兴昊 <apple@appledeiMac.local>
2018-01-31 19:39:23 +0800

    优化

--- app/models/Rooms.php
+++ app/models/Rooms.php
@@ -285,6 +285,14 @@ class Rooms extends BaseModel
 
         $user_ids = $hot_cache->zrevrange($key, $offset, $offset + $per_page - 1);
         $users = Users::findByIds($user_ids);
+
+        foreach ($users as $user) {
+            if ($user->isisManager($this) && USER_ROLE_MANAGER != $user->user_role) {
+                $user->user_role = USER_ROLE_MANAGER;
+                $user->update();
+            }
+        }
+
         $pagination = new PaginationModel($users, $total_entries, $page, $per_page);
         $pagination->clazz = 'Users';
 
