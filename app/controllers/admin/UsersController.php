71d7941ca49a2591ff064b032e6ab5e338c84e21
梅兴昊 <apple@appledeiMac.local>
2018-02-01 22:21:17 +0800

    优化

--- app/controllers/admin/UsersController.php
+++ app/controllers/admin/UsersController.php
@@ -212,15 +212,22 @@ class UsersController extends BaseController
     function statRoomTimeAction()
     {
         $per_page = $this->params('per_page', 30);
+        $user_id = $this->params('id');
         $page = $this->params('page');
         $stat_at = $this->params('stat_at', date('Y-m-d'));
         $stat_at = strtotime($stat_at);
         beginOfDay($stat_at);
-        $total_key = \Users::generateStatRoomTimeKey('total', $stat_at);
-        $db = \Users::getUserDb();
-        $total_entries = $db->zcard($total_key);
-        $offset = $per_page * ($page - 1);
-        $user_ids = $db->zrevrange($total_key, $offset, $offset + $per_page - 1);
+
+        if ($user_id) {
+            $user_ids = [$user_id];
+        } else {
+            $total_key = \Users::generateStatRoomTimeKey('total', $stat_at);
+            $db = \Users::getUserDb();
+            $total_entries = $db->zcard($total_key);
+            $offset = $per_page * ($page - 1);
+            $user_ids = $db->zrevrange($total_key, $offset, $offset + $per_page - 1);
+        }
+
         $users = \Users::findByIds($user_ids);
 
         foreach ($users as $user) {
