8b0f634548d34993bed5b0e3f61eaca749451b7c
梅兴昊 <apple@appledeiMac.local>
2018-02-06 19:48:07 +0800

    优化

--- app/models/Users.php
+++ app/models/Users.php
@@ -1675,44 +1675,47 @@ class Users extends BaseModel
         if ($rand_num <= 50) {
             $room->pushTopTopicMessage($user);
         } elseif (50 < $rand_num && $rand_num <= 80) {
-            $gift_num = mt_rand(1, 15);
-            $gifts = Gifts::findForeach();
-            $gift_ids = [];
 
-            foreach ($gifts as $gift) {
-                $gift_ids[] = $gift->id;
-            }
+            if ($room->getRealUserNum() > 0) {
+                $gift_num = mt_rand(1, 15);
+                $gifts = Gifts::findForeach();
+                $gift_ids = [];
 
-            $index = array_rand($gift_ids);
-            $gift_id = $gift_ids[$index];
-            $gift = Gifts::findFirstById($gift_id);
-
-            if ($user->canGiveGift($gift, $gift_num)) {
-
-                $receiver = $room->findRealUser();
-
-                if ($receiver) {
-                    $give_result = GiftOrders::giveTo($user_id, $receiver->id, $gift, $gift_num);
-
-                    if ($give_result) {
-                        $room->pushGiftMessage($user, $receiver, $gift, $gift_num);
-                    }
+                foreach ($gifts as $gift) {
+                    $gift_ids[] = $gift->id;
                 }
 
-            } else {
-                info("can not send gift", $user->id, $room->id, $gift_id, $gift_num, $user->diamond);
+                $index = array_rand($gift_ids);
+                $gift_id = $gift_ids[$index];
+                $gift = Gifts::findFirstById($gift_id);
+
+                if ($user->canGiveGift($gift, $gift_num)) {
+
+                    $receiver = $room->findRealUser();
+
+                    if ($receiver) {
+                        $give_result = GiftOrders::giveTo($user_id, $receiver->id, $gift, $gift_num);
+
+                        if ($give_result) {
+                            $room->pushGiftMessage($user, $receiver, $gift, $gift_num);
+                        }
+                    }
+
+                } else {
+                    info("can not send gift", $user->id, $room->id, $gift_id, $gift_num, $user->diamond);
+                }
             }
+
         } elseif (80 < $rand_num && $rand_num <= 90) {
 
-            if ($user->current_room_seat_id > 0) {
-                return;
-            }
+            if ($user->current_room_seat_id < 1 && $room->getRealUserNum() > 0) {
 
-            $room_seat = \RoomSeats::findFirst(['conditions' => 'room_id = ' . $room->id . " and (user_id = 0 or user_id is null) and status = " . STATUS_ON]);
+                $room_seat = \RoomSeats::findFirst(['conditions' => 'room_id = ' . $room->id . " and (user_id = 0 or user_id is null) and status = " . STATUS_ON]);
 
-            if ($room_seat) {
-                $room_seat->up($user);
-                $room->pushUpMessage($user, $room_seat);
+                if ($room_seat) {
+                    $room_seat->up($user);
+                    $room->pushUpMessage($user, $room_seat);
+                }
             }
 
         } else {
