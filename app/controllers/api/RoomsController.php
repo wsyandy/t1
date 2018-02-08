a2217ca2b775078b0e94f972f10e25e6b3e2886d
梅兴昊 <apple@appledeiMac.local>
2018-02-08 14:26:41 +0800

    log

--- app/controllers/api/RoomsController.php
+++ app/controllers/api/RoomsController.php
@@ -154,7 +154,12 @@ class RoomsController extends BaseController
 
         //如果进入其他房间时 用户身上有房间 先退出房间
         $current_room = $this->currentUser()->current_room;
+
+        info($this->currentUser()->sid, $this->currentUser()->current_room_id, $room_id);
+
         if ($current_room && $current_room->id != $room_id) {
+
+            info($this->currentUser()->sid, $current_room->id, $room_id);
             $current_room->exitRoom($this->currentUser());
 
             //如果进入其他房间时 用户身上有麦位 先下麦位
