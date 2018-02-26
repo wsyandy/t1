878170a753922f7cb99590d20692f26fe798f3b7
梅兴昊 <apple@appledeiMac.local>
2018-02-26 18:12:42 +0800

    分享

---
+++ app/controllers/api/SharesController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: administrator
+ * Date: 2018/1/19
+ * Time: 下午7:52
+ */
+
+namespace api;
+
+class SharesController extends BaseController
+{
+
+    function detailAction()
+    {
+
+    }
+
+    function resultAction()
+    {
+      
+    }
+
+}
