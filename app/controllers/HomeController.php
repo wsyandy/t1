f921d0737b4efe1aa6217bc4167549ed93608ca6
梅兴昊 <apple@appledeiMac.local>
2018-03-02 17:36:19 +0800

    web

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,6 +4,25 @@ class HomeController extends \ApplicationController
 {
     function indexAction()
     {
+        $user_agent = $this->request->getUserAgent();
+        $is_pc = true;
 
+        if (preg_match('/Mobile/i', $user_agent)) {
+            $is_pc = false;
+        }
+
+        $host = $this->getHost();
+        $params = $this->params();
+        unset($params['_url']);
+
+        if ($is_pc) {
+            $forward = [
+                "namespace" => "web",
+                "controller" => "home",
+                "action" => "index",
+                "params" => $this->params()
+            ];
+            $this->dispatcher->forward($forward);
+        }
     }
 }
