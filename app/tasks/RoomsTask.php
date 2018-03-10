9a774a7016974587509e9d904ddc55a6b49eeb94
梅兴昊 <apple@appledeiMac.local>
2018-03-10 14:42:29 +0800

    沉默用户房间数量减小

--- app/tasks/RoomsTask.php
+++ app/tasks/RoomsTask.php
@@ -154,7 +154,7 @@ class RoomsTask extends \Phalcon\Cli\Task
     {
         $online_silent_room_num = Rooms::getOnlineSilentRoomNum();
 
-        if ($online_silent_room_num >= 60) {
+        if ($online_silent_room_num >= 5) {
             info("online_silent_room_num", $online_silent_room_num);
             return;
         }
