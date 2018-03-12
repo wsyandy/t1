a35900768dd41c5e782793f04bca94247e0612b7
梅兴昊 <apple@appledeiMac.local>
2018-03-12 10:43:07 +0800

    工会流水

--- app/models/Unions.php
+++ app/models/Unions.php
@@ -18,7 +18,7 @@ class Unions extends BaseModel
      */
     private $_user;
 
-    static $STATUS = [STATUS_ON => '正常', STATUS_BLOCKED => '被封', STATUS_OFF => '解散'];
+    static $STATUS = [STATUS_ON => '正常', STATUS_BLOCKED => '被封', STATUS_OFF => '解散', STATUS_PROGRESS => '创建中'];
     static $TYPE = [UNION_TYPE_PUBLIC => '工会', UNION_TYPE_PRIVATE => '家族'];
     static $AUTH_STATUS = [AUTH_SUCCESS => '审核成功', AUTH_FAIL => '审核失败', AUTH_WAIT => '等待审核'];
     static $RECOMMEND = [STATUS_ON => '是', STATUS_OFF => '否'];
@@ -30,12 +30,14 @@ class Unions extends BaseModel
 
             if (UNION_TYPE_PUBLIC == $user->union->type) {
                 return [ERROR_CODE_FAIL, '您已加入工会,不能创建家族'];
-            } else {
+            } elseif (STATUS_ON == $user->union->status || STATUS_PROGRESS == $user->status) {
                 return [ERROR_CODE_FAIL, '您已加入家族,不能创建家族'];
             }
         }
 
-        if ($user->diamond < 100) {
+        $amount = 100;
+
+        if ($user->diamond < $amount) {
             return [ERROR_CODE_FORM, '钻石余额不足'];
         }
 
@@ -70,6 +72,7 @@ class Unions extends BaseModel
         $union->mobile = $user->mobile;
         $union->type = UNION_TYPE_PRIVATE;
         $union->avatar_status = AUTH_SUCCESS;
+        $union->status = STATUS_PROGRESS;
 
         $dest_filename = APP_NAME . '/unions/avatar/' . uniqid() . '.jpg';
         $res = \StoreFile::upload($avatar_file, $dest_filename);
@@ -80,6 +83,23 @@ class Unions extends BaseModel
 
         $union->avatar = $dest_filename;
         $union->save();
+
+        $user->union_id = $union->id;
+        $user->update();
+
+        $opts = ['remark' => '创建家族,花费钻石' . $amount . "个", 'union_id' => $union->id, 'mobile' => $user->mobile];
+        $res = AccountHistories::changeBalance($user->id, ACCOUNT_TYPE_CREATE_UNION, $amount, $opts);
+
+        if ($res) {
+            $union->status = STATUS_ON;
+            return [ERROR_CODE_SUCCESS, '创建成功'];
+        }
+
+        $union->error_reason = "扣除钻石失败,创建失败";
+        $union->status = STATUS_OFF;
+        $union->update();
+
+        return [ERROR_CODE_FAIL, '创建失败'];
     }
 
     //创建公会
