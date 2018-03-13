3165725092eaf24147681507736dd35efb21d193
梅兴昊 <apple@appledeiMac.local>
2018-03-13 20:55:02 +0800

    log

--- app/config/acl.php
+++ app/config/acl.php
@@ -24,7 +24,7 @@ return [
         'sms_channels' => '*', 'soft_versions' => '*', 'client_themes' => '*', 'push_messages' => '*', 'partners' => '*',
         'channel_soft_versions' => '*', 'gifts' => '*', 'emoticon_images' => '*', 'audios' => "*", 'room_themes' => '*',
         'musics' => '*', 'banners' => '*', 'complaints' => '*', 'sms_histories' => '*', 'rooms' => '*', 'broadcasts' => '*',
-        'share_histories' => '*'
+        'share_histories' => '*', 'audio_chapters' => '*'
     ],
 
     'product_operator' => ['dashboard' => '*', 'users' => '*', 'devices' => ['index', 'edit', 'update'],
