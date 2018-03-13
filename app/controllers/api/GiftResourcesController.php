093525ef52d579943c7bd82736f139a4bde1c35c
梅兴昊 <apple@appledeiMac.local>
2018-03-13 23:16:50 +0800

    log

--- app/controllers/api/GiftResourcesController.php
+++ app/controllers/api/GiftResourcesController.php
@@ -15,7 +15,15 @@ class GiftResourcesController extends BaseController
     {
         $gift_resource = \GiftResources::findFirstBy(['status' => STATUS_ON], 'id desc');
 
+        $resource_file_url = '';
+        $resource_code = '';
+
+        if (!$gift_resource) {
+            $resource_file_url = $gift_resource->resourceFileUrl();
+            $resource_code = $gift_resource->resource_code;
+        }
+
         return $this->renderJSON(ERROR_CODE_SUCCESS, '',
-            ['resource_file_url' => $gift_resource->resourceFileUrl(), 'resource_code' => $gift_resource->resource_code]);
+            ['resource_file_url' => $resource_file_url, 'resource_code' => $resource_code]);
     }
 }
