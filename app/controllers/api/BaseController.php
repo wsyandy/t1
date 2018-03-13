07e3cd7f3897bb5d3f5d73665e512b845acbb995
梅兴昊 <apple@appledeiMac.local>
2018-03-13 14:22:46 +0800

    log

--- app/controllers/api/BaseController.php
+++ app/controllers/api/BaseController.php
@@ -440,7 +440,7 @@ class BaseController extends ApplicationController
 
         if (isDevelopmentEnv()) {
             debug('base_valid_sign_signature_error', $sign_str);
-            return [false, "Sign error! md5(md5($sign_str) + $ckey) sign=$sign"];
+            return [false, "Sign error! md5(md5($sign_str) + $ckey) sign=$sign client_sign=" . $this->params('h')];
         }
 
         return [false, t('base_valid_sign_signature_error')];
