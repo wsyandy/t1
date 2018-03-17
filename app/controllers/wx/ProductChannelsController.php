537fd8e304bebed10d0d08e3b0715fd141a85f15
梅兴昊 <apple@appledeiMac.local>
2018-03-17 17:38:53 +0800

    公众号

---
+++ app/controllers/wx/ProductChannelsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2017/12/26
+ * Time: 下午5:18
+ */
+
+namespace wx;
+
+class ProductChannelsController extends BaseController
+{
+    function strategiesAction()
+    {
+        $this->view->title = '玩转Hi';
+    }
+}
