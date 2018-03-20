86ccc581cef4ac4ab002e9cc98578274081f2aa3
杨星 <apple@appledeiMac.local>
2018-03-20 19:19:53 +0800

    debug

--- app/models/Unions.php
+++ app/models/Unions.php
@@ -499,7 +499,7 @@ class Unions extends BaseModel
                 Chats::sendTextSystemMessage($user->id, $content);
             } else {
                 $content = "$user->nickname" . "已经退出了家族";
-                Chats::sendTextSystemMessage($user->id, $content);
+                Chats::sendTextSystemMessage($this->user_id, $content);
             }
         }
 
