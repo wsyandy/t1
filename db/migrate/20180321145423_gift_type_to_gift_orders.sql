78e78b69b304ed8c85f05081b74376d794b636e5
梅兴昊 <apple@appledeiMac.local>
2018-03-21 15:03:53 +0800

    后台优化

--- db/migrate/20180321145423_gift_type_to_gift_orders.sql
+++ db/migrate/20180321145423_gift_type_to_gift_orders.sql
@@ -1,6 +1,6 @@
-ALTER TABLE gift_ordrs ADD COLUMN gift_type INTEGER ;
+ALTER TABLE gift_orders ADD COLUMN gift_type INTEGER ;
 ALTER TABLE user_gifts ADD COLUMN gift_type INTEGER ;
 
 
-CREATE index gift_type_on_gift_ordrs on gift_ordrs(gift_type);
+CREATE index gift_type_on_gift_orders on gift_orders(gift_type);
 CREATE index gift_type_on_user_gifts on user_gifts(gift_type);
