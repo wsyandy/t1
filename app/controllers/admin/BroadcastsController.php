f661a769b874884ad44afb80063893cffb569c5d
梅兴昊 <apple@appledeiMac.local>
2018-03-24 20:27:24 +0800

    后台优化

--- app/controllers/admin/BroadcastsController.php
+++ app/controllers/admin/BroadcastsController.php
@@ -100,7 +100,7 @@ class BroadcastsController extends BaseController
             }
             \OperatingRecords::logBeforeUpdate($this->currentOperator(), $room);
             if ($room->update()) {
-                return $this->renderJSON(ERROR_CODE_SUCCESS, '编辑成功', ['error_url' => '/admin/broadcasts']);
+                return $this->renderJSON(ERROR_CODE_SUCCESS, '编辑成功', ['room' => $room->toDetailJson()]);
             } else {
                 return $this->renderJSON(ERROR_CODE_FAIL, '编辑失败');
             }
