8518479d218fac276699ccc5ee1399c85a786ce3
门月锋 <menyuefeng@mobase.cn>
2018-03-26 23:27:15 +0800

    关注上上线通知

--- app/models/Users.php
+++ app/models/Users.php
@@ -116,7 +116,10 @@ class Users extends BaseModel
             $this->updateLastAt();
 
             //好友上线提醒(每小时选取最新的一个好友上线提醒)
-            $this->pushonlineRemind();
+            $this->pushFriendOnlineRemind();
+
+            //关注的人上线提醒(每小时选取最新关注的人上线提醒)
+            $this->pushFollowOnlineRemind();
         }
 
         if ($this->hasChanged('ip') && $this->ip) {
