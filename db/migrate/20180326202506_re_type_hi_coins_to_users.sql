21eb9c919bc6ac3092591e3a4616beb6ebd2f507
梅兴昊 <apple@appledeiMac.local>
2018-03-26 21:58:31 +0800

    优化

---
+++ db/migrate/20180326202506_re_type_hi_coins_to_users.sql
@@ -0,0 +1,3 @@
+alter TABLE users ALTER COLUMN hi_coins type DECIMAL (10, 4);
+alter TABLE hi_coin_histories ALTER COLUMN hi_coins type DECIMAL (10, 4);
+alter TABLE hi_coin_histories ALTER COLUMN balance type DECIMAL (10, 4);
