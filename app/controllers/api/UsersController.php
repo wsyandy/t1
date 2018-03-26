deacbbf24fbf1477724dfc404803b0c981e37dcf
杨星 <apple@appledeiMac.local>
2018-03-26 17:50:15 +0800

    优化

--- app/controllers/api/UsersController.php
+++ app/controllers/api/UsersController.php
@@ -573,9 +573,9 @@ class UsersController extends BaseController
         $user = $this->currentUser();
         $gold = $user->addSignInHistory();
         if ($gold) {
-            return $this->renderJSON(ERROR_CODE_SUCCESS, '');
+            return $this->renderJSON(ERROR_CODE_SUCCESS, '', $gold);
         } else {
-            return $this->renderJSON(ERROR_CODE_FAIL, '已签到');
+            return $this->renderJSON(ERROR_CODE_FAIL, '已签到', $gold);
         }
     }
 
