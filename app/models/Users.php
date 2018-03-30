4263ba326c3a85288a9cb5806c95cbe1bf78b992
门月锋 <menyuefeng@mobase.cn>
2018-03-30 22:07:55 +0800

    提醒

--- app/models/Users.php
+++ app/models/Users.php
@@ -129,11 +129,13 @@ class Users extends BaseModel
         if ($this->hasChanged('last_at')) {
             $this->updateLastAt();
 
-            //好友上线提醒(每小时选取最新的一个好友上线提醒)
-            $this->pushFriendOnlineRemind();
+            if (isDevelopmentEnv()) {
+                //好友上线提醒(每小时选取最新的一个好友上线提醒)
+                $this->pushFriendOnlineRemind();
 
-            //关注的人上线提醒(每小时选取最新关注的人上线提醒)
-            $this->pushFollowedOnlineRemind();
+                //关注的人上线提醒(每小时选取最新关注的人上线提醒)
+                $this->pushFollowedOnlineRemind();
+            }
         }
 
         if ($this->hasChanged('ip') && $this->ip) {
