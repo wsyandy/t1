5a854f945e6b9c62cbbdd52476f6580049212a46
邢宇 <xingyu@xianjinshandai.cn>
2018-03-31 11:42:48 +0800

    查到所有公司ip用户，所属组织刷成公司

---
+++ app/tasks/XingYuTask.php
@@ -0,0 +1,15 @@
+<?php
+
+class YuTask extends \Phalcon\Cli\Task
+{
+    function companyIpAction()
+    {
+        $company_ip = '116.226.124.47';
+        $users = \Users::findByIp($company_ip);
+        foreach ($users as $user) {
+            $user->organisation = COMPANY;
+            $user->update();
+            info('id:' . $user->id, '，员工昵称：' . $user->nickname, '，所属组织：', $user->organisation,'ip地址：'.$user->ip);
+        }
+    }
+}
