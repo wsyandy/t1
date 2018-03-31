01548b940a44b69f141c4d68048e2644ba3d6ece
康广庆 <kanggq@365yf.com>
2018-03-31 22:44:53 +0800

    游戏

--- app/controllers/m/GamesController.php
+++ app/controllers/m/GamesController.php
@@ -41,9 +41,7 @@ class GamesController extends BaseController
     // 提交入场费
     function feeAction()
     {
-
         $room_id = $this->currentUser()->current_room_id > 0 ? $this->currentUser()->current_room_id : $this->currentUser()->room_id;
-        info('全部参数', $this->params());
         $room_info_key = "game_room_" . $room_id . '_info';
         $hot_cache = \Rooms::getHotWriteCache();
         $info = $hot_cache->hgetall($room_info_key);
@@ -67,16 +65,10 @@ class GamesController extends BaseController
 
         if ($pay_type == 'diamond' && $current_user->diamond < $amount) {
             return $this->renderJSON(ERROR_CODE_FAIL, '钻石不足');
-        } else {
-            $current_user->diamond -= $amount;
-            $current_user->update();
         }
 
         if ($pay_type == 'gold' && $current_user->gold < $amount) {
             return $this->renderJSON(ERROR_CODE_FAIL, '金币不足');
-        } else {
-            $current_user->gold -= $amount;
-            $current_user->update();
         }
 
         return $this->renderJSON(ERROR_CODE_SUCCESS, '');
@@ -88,8 +80,7 @@ class GamesController extends BaseController
 
         $room_info_key = "game_room_" . $room_id . '_info';
         $hot_cache = \Rooms::getHotWriteCache();
-        $info = $hot_cache->hgetall($room_info_key);
-        $room_host_id = fetch($info, 'room_host_id');
+        $room_host_id = $hot_cache->hget($room_info_key, 'room_host_id');
         $body = [];
         $body['user_id'] = $this->currentUser()->id;
         $body['source'] = $this->currentProductChannel()->code;
@@ -124,9 +115,18 @@ class GamesController extends BaseController
         $room_id = $this->currentUser()->current_room_id > 0 ? $this->currentUser()->current_room_id : $this->currentUser()->room_id;
         $room_info_key = "game_room_" . $room_id . '_info';
         $hot_cache = \Rooms::getHotWriteCache();
-        $can_enter = $hot_cache->hget($room_info_key, 'can_enter');
+        $info = $hot_cache->hgetall($room_info_key);
+        $room_host_id = fetch($info, 'room_host_id');
+        $pay_type = fetch($info, 'pay_type');
+        $amount = fetch($info, 'amount');
+        $can_enter = fetch($info, 'can_enter');
         $data = $users->toJson('users', 'toSimpleJson');
         $data['can_enter'] = intval($can_enter);
+
+        //扣除入场费
+        if($can_enter){
+
+        }
 
         return $this->renderJSON(ERROR_CODE_SUCCESS, '', $data);
     }
@@ -136,6 +136,12 @@ class GamesController extends BaseController
         $room_id = $this->currentUser()->current_room_id > 0 ? $this->currentUser()->current_room_id : $this->currentUser()->room_id;
         $room_info_key = "game_room_" . $room_id . '_info';
         $hot_cache = \Rooms::getHotWriteCache();
+        $info = $hot_cache->hgetall($room_info_key);
+        $room_host_id = fetch($info, 'room_host_id');
+        $pay_type = fetch($info, 'pay_type');
+        $amount = fetch($info, 'amount');
+        //扣除入场费
+
         $hot_cache->hset($room_info_key, 'can_enter', 1);
 
         return $this->renderJSON(ERROR_CODE_SUCCESS, '');
