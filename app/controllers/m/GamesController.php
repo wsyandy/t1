a3e322225e7ae4c775edbefd9930bc73c6c0625f
康广庆 <kanggq@365yf.com>
2018-04-02 23:43:46 +0800

    游戏

--- app/controllers/m/GamesController.php
+++ app/controllers/m/GamesController.php
@@ -42,7 +42,7 @@ class GamesController extends BaseController
             $amount = fetch($info, 'amount');
         }
 
-        info($this->currentUser()->id, $room_host_id, 'role', $this->currentUser()->user_role, $this->currentUser()->current_room_id, $room_key, 'num', $num, $pay_type, $amount);
+        info($this->currentUser()->id, 'host', $room_host_id, 'role', $this->currentUser()->user_role, $this->currentUser()->current_room_id, $room_key, 'num', $num, $pay_type, $amount);
 
         $this->view->current_user = $this->currentUser();
         $this->view->room_host_id = $room_host_id;
