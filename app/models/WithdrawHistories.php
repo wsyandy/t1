3de8f7f8b777f2310c3736498a1a4524643e8cef
梅兴昊 <apple@appledeiMac.local>
2018-04-03 22:10:38 +0800

    log

--- app/models/WithdrawHistories.php
+++ app/models/WithdrawHistories.php
@@ -187,8 +187,10 @@ class WithdrawHistories extends BaseModel
     {
         $withdraw_history = WithdrawHistories::findFirst(
             [
-                'conditions' => 'user_id = :user_id: and type = :type: and created_at >= :start: and created_at <= :end:',
-                'bind' => ['user_id' => $user->id, 'type' => WITHDRAW_TYPE_USER, 'start' => beginOfWeek(), 'end' => endOfWeek()],
+                'conditions' => '(user_id = :user_id: and type = :type: and created_at >= :start: and created_at <= :end:) or '
+                    . ' (status = :status:)',
+                'bind' => ['user_id' => $user->id, 'type' => WITHDRAW_TYPE_USER, 'start' => beginOfWeek(), 'end' => endOfWeek(),
+                    'status' => WITHDRAW_STATUS_WAIT],
                 'order' => 'id desc'
             ]
         );
