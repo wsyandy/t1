20b20af24ad9d5f023c873741983214b113eb61a
门月锋 <menyuefeng@mobase.cn>
2018-04-03 11:01:31 +0800

    提醒

--- app/controllers/api/RoomsController.php
+++ app/controllers/api/RoomsController.php
@@ -208,13 +208,11 @@ class RoomsController extends BaseController
         $key = $this->currentProductChannel()->getChannelKey($room->channel_name, $this->currentUser()->id);
         $app_id = $this->currentProductChannel()->getImAppId();
 
-        if (isDevelopmentEnv()) {
-            //好友上线开播提醒(同一个用户一个小时之内只提醒一次)
-            $this->currentUser()->pushFriendIntoRoomRemind();
+        //好友上线开播提醒(同一个用户一个小时之内只提醒一次)
+        $this->currentUser()->pushFriendIntoRoomRemind();
 
-            //关注的人开播提醒(同一个用户一个小时之内只提醒一次)
-            $this->currentUser()->pushFollowedIntoRoomRemind();
-        }
+        //关注的人开播提醒(同一个用户一个小时之内只提醒一次)
+        $this->currentUser()->pushFollowedIntoRoomRemind();
 
         $res = $room->toJson();
         $res['channel_key'] = $key;
