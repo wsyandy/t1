d43de8c0c3d47c2faca42ddbb6f5cea2937efd57
康广庆 <kanggq@365yf.com>
2018-04-03 11:32:40 +0800

    统计

--- app/controllers/admin/ActiveUsersController.php
+++ app/controllers/admin/ActiveUsersController.php
@@ -7,19 +7,21 @@ class ActiveUsersController extends BaseController
     function dayRankListAction()
     {
         $start_at = $this->params('start_at', date('Y-m-d', beginOfDay()));
-        $find_at = strtotime($start_at);
-        $stop_at = endOfDay($find_at);
+        $start_at = beginOfDay(strtotime($start_at));
+        $end_at = endOfDay($start_at);
         $active_user_number = [];
-        $hot_cache = \Users::getHotWriteCache();
+        $stat_db = \Stats::getStatDb();
 
-        for ($find_at_dot = $find_at; $find_at_dot <= $stop_at; $find_at_dot += 600) {
-            $key = 'online_user_list_' . date('YmdHi', $find_at_dot);
-            $portion_active_user_number = $hot_cache->zcard($key);
-            $time = date('Y-m-d H:i:s',$find_at_dot);
-            $active_user_number[$time] = $portion_active_user_number;
+        for ($start_at_dot = $start_at; $start_at_dot <= $end_at; $start_at_dot += 600) {
+            $key = 'online_user_list_' . date('YmdHi', $start_at_dot);
+            $portion_active_user_number = $stat_db->zcard($key);
+            info($key, $portion_active_user_number);
+            $time = date('Y-m-d H:i:s', $start_at_dot);
+            $active_user_number[$time] = intval($portion_active_user_number);
         }
+
         $this->view->start_at = $start_at;
-        $this->view->active_user_number = json_encode($active_user_number,JSON_UNESCAPED_UNICODE);
+        $this->view->active_user_number = json_encode($active_user_number, JSON_UNESCAPED_UNICODE);
     }
 
 }
