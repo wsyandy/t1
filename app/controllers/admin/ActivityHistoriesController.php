c01a5b05cfb1cef5eaf0c36215394c125b0bd786
梅兴昊 <meixinghao@365yf.com>
2018-04-06 15:35:52 +0800

    优化

--- app/controllers/admin/ActivityHistoriesController.php
+++ app/controllers/admin/ActivityHistoriesController.php
@@ -16,7 +16,9 @@ class ActivityHistoriesController extends BaseController
         $page = $this->params('page');
         $activity_histories = \ActivityHistories::findPagination(['conditions' => 'user_id = :user_id:',
             'bind' => ['user_id' => $user_id]], $page, 20);
+
         $this->view->activity_histories = $activity_histories;
+        $this->view->activity_prize_types = \Activities::$ACTIVITY_PRIZE_TYPE;
     }
 
     function indexAction()
@@ -25,13 +27,19 @@ class ActivityHistoriesController extends BaseController
 
         $start_at = beginOfDay(strtotime($stat_at));
         $end_at = endOfDay(strtotime($stat_at));
-
         $activity_id = $this->params('activity_id');
+        $prize_type = $this->params('prize_type');
         $activity = \Activities::findFirstById($activity_id);
+
         $cond = ['conditions' => 'activity_id = :activity_id: and created_at >= :start: and created_at <= :end:',
             'bind' => ['activity_id' => $activity->id, 'start' => $start_at, 'end' => $end_at],
             'order' => 'id desc'
         ];
+
+        if ($prize_type) {
+            $cond['conditions'] .= " and prize_type = :prize_type:";
+            $cond['bind']['prize_type'] = $prize_type;
+        }
 
         $page = $this->params('page');
         $activity_histories = \ActivityHistories::findPagination($cond, $page, 30);
@@ -39,5 +47,8 @@ class ActivityHistoriesController extends BaseController
         $this->view->activity_histories = $activity_histories;
         $this->view->stat_at = $stat_at;
         $this->view->activity_id = $activity_id;
+        $this->view->prize_type = $prize_type;
+        $this->view->activity_prize_types = \Activities::$ACTIVITY_PRIZE_TYPE;
+        $this->view->total_num = $activity_histories->total_entries;
     }
 }
