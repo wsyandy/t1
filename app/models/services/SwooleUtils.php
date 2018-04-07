91f2b7fbcbeac0c32903e018d236c30ba0008245
梅兴昊 <apple@appledeiMac.local>
2018-04-07 13:59:03 +0800

    测试

--- app/models/services/SwooleUtils.php
+++ app/models/services/SwooleUtils.php
@@ -172,7 +172,7 @@ class SwooleUtils extends \BaseModel
         info($port, $ip, $action, $payload);
 
         try {
-            $client = new SwooleClient($ip, $port, 1);
+            $client = new SwooleClient($ip, $port, 3);
             if (!$client->connect()) {
                 info("Exce connect fail", $ip, $port, $payload);
                 return false;
