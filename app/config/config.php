dcc2d4c245ff8285af7ea608e2c476a9be575761
kangguangqing <gqkang@365yf.com>
2018-04-10 23:48:48 +0800

    异步

--- app/config/config.php
+++ app/config/config.php
@@ -13,7 +13,7 @@ return new \Phalcon\Config([
     'hot_cache_endpoints' => env('hot_cache_endpoints', 'redis://127.0.0.1:6379/' . APP_NAME),
     'cache_endpoint' => env('cache_endpoint', 'redis://127.0.0.1:6379/' . APP_NAME),
     'job_queue' => ['endpoint' => env('job_queue_endpoint', 'redis://127.0.0.1:6379/job_queue_' . APP_NAME),
-        'tubes' => ['default' => isProduction() ? 16 : 3]],
+        'tubes' => ['default' => isProduction() ? 12 : 3]],
     'user_db_endpoints' => env('user_db_endpoints', 'ssdb://127.0.0.1:8888/' . APP_NAME),
     'stat_db' => env('stat_db', 'ssdb://127.0.0.1:8888/' . APP_NAME),
     'redlock_endpoints' => env('redlock_endpoints', 'redis://127.0.0.1:6379/' . APP_NAME),
