7e661ef603e9e679cd0d4f8bca260d6fefcef453
门月锋 <menyuefeng@mobase.cn>
2018-04-11 20:03:41 +0800

    礼物

---
+++ app/controllers/iapi/GiftResourcesController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2018/1/3
+ * Time: 下午5:04
+ */
+
+namespace iapi;
+
+class GiftResourcesController extends BaseController
+{
+
+    function indexAction()
+    {
+        $gift_resource = \GiftResources::findFirstBy(['status' => STATUS_ON], 'id desc');
+
+        $resource_file_url = '';
+        $resource_code = '';
+
+        if ($gift_resource) {
+            $resource_file_url = $gift_resource->resourceFileUrl();
+            $resource_code = $gift_resource->resource_code;
+        }
+
+        return $this->renderJSON(ERROR_CODE_SUCCESS, '',
+            ['resource_file_url' => $resource_file_url, 'resource_code' => $resource_code]);
+    }
+}
---
+++ app/controllers/iapi/UserGiftsController.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maoluanjuan
+ * Date: 09/01/2018
+ * Time: 22:35
+ */
+
+namespace iapi;
+
+class UserGiftsController extends BaseController
+{
+    function indexAction()
+    {
+        $user_id = $this->params('user_id');
+        $car_gift = $this->params('car_gift', 0); //座驾礼物
+        $common_gift = $this->params('common_gift', 0); //普通礼物
+
+        if (isBlank($user_id)) {
+            $user_id = $this->currentUserId();
+        }
+
+        $user = \Users::findFirstById($user_id);
+
+        if (!$user) {
+            return $this->renderJSON(ERROR_CODE_FAIL, '请求错误');
+        }
+
+        $total_gift_num = $user->getReceiveGiftNum();
+        $opts = [];
+
+        if (!$car_gift && !$common_gift) {
+            $conds = ['conditions' => 'user_id = ' . $user_id, 'order' => 'amount desc'];
+
+            $page = $this->params('page', 1);
+            $per_page = $this->params('per_page', 100);
+
+            $user_gifts = \UserGifts::findPagination($conds, $page, $per_page);
+
+            $opts = $user_gifts->toJson('user_gifts', 'toJson');
+
+        } else {
+
+            if ($car_gift) {
+                $opts['user_car_gifts'] = \UserGifts::searchCarGifts($user_id);
+            }
+
+            if ($common_gift) {
+                $opts['user_gifts'] = \UserGifts::searchCommonGifts($user_id);
+            }
+        }
+
+        $opts = array_merge(['total_gift_num' => $total_gift_num], $opts);
+
+        return $this->renderJSON(ERROR_CODE_SUCCESS, '', $opts);
+    }
+}
