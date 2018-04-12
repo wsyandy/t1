129a22277cad175c61ea22e19390245883a3c352
梅兴昊 <meixinghao@365yf.com>
2018-04-13 02:27:36 +0800

    优化

--- app/models/HiCoinHistories.php
+++ app/models/HiCoinHistories.php
@@ -245,22 +245,23 @@ class HiCoinHistories extends BaseModel
         $hi_coin_history->country_id = $user->country_id;
         $hi_coin_history->union_id = $user->union_id;
         $hi_coin_history->union_type = $user->union_type;
-        $hi_coin_history->save();
 
-        $opts = ['remark' => $remark, 'hi_coin_history_id' => $hi_coin_history->id];
-        info('user_id', $user->id, $opts);
+        if ($hi_coin_history->save()) {
 
-        if ($hi_coin_history->gold > 0) {
-            \GoldHistories::changeBalance($user->id, GOLD_TYPE_HI_COIN_EXCHANGE_DIAMOND, $gold, $opts);
+            $opts = ['remark' => $remark, 'hi_coin_history_id' => $hi_coin_history->id];
+            info('user_id', $user->id, $opts);
+
+            if ($hi_coin_history->gold > 0) {
+                \GoldHistories::changeBalance($user->id, GOLD_TYPE_HI_COIN_EXCHANGE_DIAMOND, $gold, $opts);
+            }
+
+            if ($hi_coin_history->diamond > 0) {
+                \AccountHistories::changeBalance($user->id, ACCOUNT_TYPE_HI_COIN_EXCHANGE_DIAMOND, $diamond, $opts);
+            }
+
+            $user->hi_coins = $hi_coin_history->balance;
+            $user->update();
         }
-
-        if ($hi_coin_history->diamond > 0) {
-            \AccountHistories::changeBalance($user->id, ACCOUNT_TYPE_HI_COIN_EXCHANGE_DIAMOND, $diamond, $opts);
-        }
-
-        $user->hi_coins = $hi_coin_history->balance;
-        $user->update();
-
 
         unlock($lock);
 
