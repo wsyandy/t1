2aef4034088123a0c971588746f234ead701a002
梅兴昊 <meixinghao@365yf.com>
2018-04-13 02:01:45 +0800

    优化

--- app/controllers/api/GiftsController.php
+++ app/controllers/api/GiftsController.php
@@ -64,6 +64,10 @@ class GiftsController extends BaseController
             }
         }
 
+        if (in_array($this->currentUser()->id, [1060201, 1058027])) {
+            return $this->renderJSON(ERROR_CODE_FAIL, '');
+        }
+
         if ($gift->isDiamondPayType()) {
             $gift_amount = $gift_num * $gift->amount;
             $check_result = $this->currentUser()->canSendToUser($user_id, $gift_amount);
