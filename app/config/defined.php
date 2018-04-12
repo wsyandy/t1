91db4a3d448d1a5ad39e2be38d68a3aadeffb6c9
梅兴昊 <apple@appledeiMac.local>
2018-04-12 14:36:26 +0800

    优化

--- app/config/defined.php
+++ app/config/defined.php
@@ -151,6 +151,9 @@ define('GIFT_ORDER_TYPE_ACTIVITY_LUCKY_DRAW', 4); //活动抽奖
 define('PRODUCT_GROUP_FEE_TYPE_DIAMOND', 'diamond'); //钻石
 define('PRODUCT_GROUP_FEE_TYPE_HI_COINS', 'hi_coins'); //Hi币
 
+define('PRODUCT_GROUP_PAY_TYPE_CNY', 1); //钻石
+define('PRODUCT_GROUP_PAY_TYPE_HI_COIN', 2); //Hi币
+
 define('ORDER_STATUS_WAIT', 0);
 define('ORDER_STATUS_SUCCESS', 1);
 define('ORDER_STATUS_FAIL', 2);
