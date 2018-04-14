af74d32cb52a584f80f420ce840a52c0dd41001f
梅兴昊 <apple@appledeiMac.local>
2018-04-14 22:25:46 +0800

    优化

--- app/controllers/admin/AlbumsController.php
+++ app/controllers/admin/AlbumsController.php
@@ -16,12 +16,24 @@ class AlbumsController extends BaseController
         $per_page = $this->params('per_page', 30);
         $user_id = $this->params('user_id');
         $auth_status = $this->params('auth_status');
+        $auth_type = $this->params('auth_type');
         $cond = ['conditions' => 'user_id =' . $user_id, 'order' => 'id asc'];
 
         if ($auth_status) {
             $cond['conditions'] .= " and auth_status = $auth_status";
         }
 
+        $hot_cache = \Albums::getHotWriteCache();
+
+        if ($auth_type) {
+            $ids = $hot_cache->zrange("albums_auth_type_{$auth_type}_list_user_id_" . $user_id, 0, -1);
+
+            if (count($ids) > 0) {
+                $cond['conditions'] .= ' and id in (' . implode(',', $ids) . ')';
+            }
+        }
+
+        debug($cond);
         $albums = \Albums::findPagination($cond, $page, $per_page);
 
         $this->view->albums = $albums;
@@ -37,26 +49,14 @@ class AlbumsController extends BaseController
         $album = \Albums::findFirstById($album_id);
         $album->auth_status = $auth_status;
 
-        //选择头像使用
+        $hot_cache = \Albums::getHotWriteCache();
+
         if (1 == $album->user_id) {
-            $hot_cache = \Albums::getHotWriteCache();
+
             $auth_type = $this->params('auth_type');
 
             if ($auth_type) {
-
-                if ($auth_type == 1) {
-                    $hot_cache->zrem("albums_auth_type_2_list_user_id_" . $album->user_id, $album->id);
-                    $hot_cache->zrem("albums_auth_type_3_list_user_id_" . $album->user_id, $album->id);
-                } elseif ($auth_type == 2) {
-                    $hot_cache->zrem("albums_auth_type_1_list_user_id_" . $album->user_id, $album->id);
-                    $hot_cache->zrem("albums_auth_type_3_list_user_id_" . $album->user_id, $album->id);
-                } else {
-                    $hot_cache->zrem("albums_auth_type_1_list_user_id_" . $album->user_id, $album->id);
-                    $hot_cache->zrem("albums_auth_type_2_list_user_id_" . $album->user_id, $album->id);
-                }
-
                 $hot_cache->zadd("albums_auth_type_{$auth_type}_list_user_id_" . $album->user_id, time(), $album->id);
-                $hot_cache->zadd("albums_auth_type_total_list_user_id_" . $album->user_id, time(), $album->id);
                 return $this->renderJSON(ERROR_CODE_SUCCESS, '');
             }
 
@@ -83,22 +83,9 @@ class AlbumsController extends BaseController
             $album = \Albums::findFirstById($album_id);
             $album->auth_status = $auth_status;
 
-            //选择头像使用
             if (1 == $album->user_id) {
+
                 if ($auth_type) {
-
-                    if ($auth_type == 1) {
-                        $hot_cache->zrem("albums_auth_type_2_list_user_id_" . $album->user_id, $album->id);
-                        $hot_cache->zrem("albums_auth_type_3_list_user_id_" . $album->user_id, $album->id);
-                    } elseif ($auth_type == 2) {
-                        $hot_cache->zrem("albums_auth_type_1_list_user_id_" . $album->user_id, $album->id);
-                        $hot_cache->zrem("albums_auth_type_3_list_user_id_" . $album->user_id, $album->id);
-                    } else {
-                        $hot_cache->zrem("albums_auth_type_1_list_user_id_" . $album->user_id, $album->id);
-                        $hot_cache->zrem("albums_auth_type_2_list_user_id_" . $album->user_id, $album->id);
-                    }
-
-                    $hot_cache->zadd("albums_auth_type_total_list_user_id_" . $album->user_id, time(), $album->id);
                     $hot_cache->zadd("albums_auth_type_{$auth_type}_list_user_id_" . $album->user_id, time(), $album->id);
                     continue;
                 }
