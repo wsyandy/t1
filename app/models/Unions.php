7c6e56cbddf7510a9eca1c1473c97e5bb2eb8b6e
杨星 <apple@appledeiMac.local>
2018-04-14 16:16:11 +0800

    debug

--- app/models/Unions.php
+++ app/models/Unions.php
@@ -637,7 +637,7 @@ class Unions extends BaseModel
             return [ERROR_CODE_FAIL, '您无此权限'];
         }
 
-        $union_history = UnionHistories::findFirst(
+        $union_history = UnionHistories::findFirstBy(
             ['user_id' => $user->id, 'union_id' => $this->id], 'id desc');
 
         $expire_at = time() - 86400 * 7;
