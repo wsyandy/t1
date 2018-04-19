9f2067d7ed71d57a9bb4e81207f448c038257455
邢宇 <xingyu@xianjinshandai.cn>
2018-04-19 10:58:56 +0800

    Debug

--- app/models/WithdrawAccounts.php
+++ app/models/WithdrawAccounts.php
@@ -99,20 +99,20 @@ class WithdrawAccounts extends BaseModel
             }
         }
 
-        $first_withdraw_account = self::findFirstWithdrawAccount($user);
+        $first_withdraw_account = self::findFirstWithdrawAccount($user->id);
 
-        if (isPresent($first_withdraw_account) && $first_withdraw_account->status == STATUS_ON) {
+        if (isPresent($first_withdraw_account)) {
             return $first_withdraw_account;
         }
 
         return 0;
     }
 
-    static function findFirstWithdrawAccount($user)
+    static function findFirstWithdrawAccount($user_id)
     {
         $withdraw_account = WithdrawAccounts::findFirst(
             [
-                'conditions' => "status = " . STATUS_ON . " and user_id = $user->id"
+                'conditions' => "status = " . STATUS_ON . " and user_id = " . $user_id
             ]
         );
 
