aeaab0a08113cc653109fb8c05efc8593e9064de
梅兴昊 <apple@appledeiMac.local>
2018-04-21 20:09:51 +0800

    违禁词

--- app/models/BannedWords.php
+++ app/models/BannedWords.php
@@ -9,13 +9,20 @@
 class BannedWords extends BaseModel
 {
 
+    static $WORD = ["骚", "嫖娼", "黄片", "毛片", "聊骚", "涉黄", "阴毛", "性爱", "做爱", "交配", "阴道", "口交", "鸡巴", "性交", "性高潮", "SM",
+        "多P", "群交", "月经", "成人", "色情", "犯罪", "诈骗", "传销", "棋牌", "彩票", "假钞", "政治", "艹你妈", "干你娘", "办理", "跪舔", "小婊砸",
+        "我日", "超赚", "领导人", "作弊", "毒品", "淫秽", "异性", "私交", "涉嫌", "欺诈", "抢购", "招人", "跪求嫖", "艹", "操B", "艹B", "男模",
+        "女模", "淫荡", "嫩模", "娇喘", "毒", "赌厅", "调情"];
+
     /**
      * @param $word
      * @return array
      */
     static function checkWord($word)
     {
+        $word = str_replace(self::$WORD, '呵呵', $word);
         return [false, $word];
+
         //临时解决
         if (mb_strlen($word) == 1) {
             return [false, $word];
