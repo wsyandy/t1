a87f4cde8ab487c19e92d0effbd169b8c0e75b60
梅兴昊 <apple@appledeiMac.local>
2018-04-23 17:39:43 +0800

    绑定手机号码

--- app/models/Users.php
+++ app/models/Users.php
@@ -3450,7 +3450,8 @@ class Users extends BaseModel
         $this->mobile = $mobile;
 
         if($this->update()) {
-            GoldHistories::changeBalance($this->id, GOLD_TYPE_BIND_MOBILE, BIND_MOBILE_GOLD);
+            $remark = "绑定手机号码奖励" . BIND_MOBILE_GOLD . "金币";
+            GoldHistories::changeBalance($this->id, GOLD_TYPE_BIND_MOBILE, BIND_MOBILE_GOLD, ['remark' => $remark]);
         }
     }
 }
