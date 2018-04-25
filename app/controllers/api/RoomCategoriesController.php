631f74d2737500e52f909ed1c70aa72b13f17e69
杨星 <apple@appledeiMac.local>
2018-04-25 19:40:46 +0800

    改变房间分类返回数据结构

--- app/controllers/api/RoomCategoriesController.php
+++ app/controllers/api/RoomCategoriesController.php
@@ -12,32 +12,19 @@ class RoomCategoriesController extends BaseController
 {
     function indexAction()
     {
-        $parent_cond = [
-            'conditions' => " status = :status: and (parent_id is null or parent_id = 0)",
+        $children_cond = [
+            'conditions' => " status = :status: and parent_id is not null and parent_id != 0",
             'bind' => ['status' => STATUS_ON],
             'order' => 'rank desc,id desc',
         ];
 
-        $room_categories = \RoomCategories::find($parent_cond);
+
+        $room_categories = \RoomCategories::find($children_cond);
 
         $room_categories_json = [];
 
         foreach ($room_categories as $room_category) {
-            $children_cond = [
-                'conditions' => " status = :status: and parent_id = :parent_id:",
-                'bind' => ['status' => STATUS_ON, 'parent_id' => $room_category->id],
-                'order' => 'rank desc,id desc',
-            ];
-
-            $second_categories = \RoomCategories::find($children_cond);
-
-            $second_categories_json = [];
-
-            foreach ($second_categories as $second_category) {
-                $second_categories_json[] = ['id' => $second_category->id, 'name' => $second_category->name];
-            }
-
-            $room_categories_json[] = ['name' => $room_category->name, 'second_categories' => $second_categories_json];
+            $second_categories_json[] = ['id' => $room_category->id, 'name' => $room_category->name];
         }
 
 
