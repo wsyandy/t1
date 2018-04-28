55acd034dabeca86967fd533e92dd969ab153fdd
梅兴昊 <apple@appledeiMac.local>
2018-04-29 02:45:00 +0800

    log

--- app/controllers/api/RoomsController.php
+++ app/controllers/api/RoomsController.php
@@ -695,26 +695,26 @@ class RoomsController extends BaseController
             } else {
 //                $search_type = '';
 
-                foreach (\Rooms::$TYPES as $key => $value) {
+//                foreach (\Rooms::$TYPES as $key => $value) {
+//
+//                    if ($type == $key) {
+//                        $search_type = $key;
+//                        break;
+//                    }
+//                }
+//
+//                if ($search_type) {
+//                    $room_category = \RoomCategories::findFirstByType($search_type);
+//
+//                    if ($room_category) {
+//                        $cond['conditions'] = " room_category_ids like :room_category_ids:";
+//                        $cond['bind']['room_category_ids'] = "%," . $room_category->id . ",%";
+//                    }
+//                }
 
-                    if ($type == $key) {
-                        $search_type = $key;
-                        break;
-                    }
-                }
-
-                if ($search_type) {
-                    $room_category = \RoomCategories::findFirstByType($search_type);
-
-                    if ($room_category) {
-                        $cond['conditions'] = " room_category_ids like :room_category_ids:";
-                        $cond['bind']['room_category_ids'] = "%," . $room_category->id . ",%";
-                    }
-                }
-
-//                $cond['conditions'] = " room_category_types like :room_category_types:";
-//                $cond['bind']['room_category_types'] = "%," . $type . ",%";
-//            }
+                $cond['conditions'] = " room_category_types like :room_category_types:";
+                $cond['bind']['room_category_types'] = "%," . $type . ",%";
+            }
         }
 
         //限制搜索条件
