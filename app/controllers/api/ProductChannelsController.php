55ac8bb491817ed441d758bb400c7ce4fae44ff6
邢宇 <xingyu@xianjinshandai.cn>
2018-05-04 17:25:04 +0800

    许愿墙

--- app/controllers/api/ProductChannelsController.php
+++ app/controllers/api/ProductChannelsController.php
@@ -36,6 +36,7 @@ class ProductChannelsController extends BaseController
             $detail_json['menu_config'][] = ['show' => true, 'title' => '测一测', 'url' => '/m/users/voice', 'icon' => $root . 'images/test.png'];
             $detail_json['menu_config'][] = ['show' => true, 'title' => '测试分销', 'url' => '/m/distribute', 'icon' => $root . 'images/test.png'];
             $detail_json['menu_config'][] = ['show' => true, 'title' => '测试砸金蛋', 'url' => '/m/draw_histories', 'icon' => $root . 'images/test.png'];
+            $detail_json['menu_config'][] = ['show' => true, 'title' => '测试砸金蛋', 'url' => '/m/wish_histories', 'icon' => $root . 'images/test.png'];
         } else {
             $detail_json['menu_config'][] = ['show' => false, 'title' => '游戏', 'url' => '/m/games', 'icon' => $root . 'images/menu_game.png'];
             if ($product_channel_id == 1) {
---
+++ app/controllers/m/WishHistoriesController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace m;
+
+class WishHistoriesController extends BaseController
+{
+    function indexAction()
+    {
+
+    }
+
+    function refreshAction()
+    {
+//        $params = $this->params();
+//        info('接收的全部参数', $params);
+//        $page = $this->params('page', 1);
+//        $per_page = 20;
+//        $cond['order'] = 'id desc';
+//        $wish_histories = \WishHistories::findPagination($cond, $page, $per_page);
+//        return $this->renderJSON(ERROR_CODE_SUCCESS, '', $wish_histories->toJson('wish_histories', 'toSimpleJson'));
+
+        $page = $this->params('page');
+        $per_page = 20;
+
+        $product_channel_id = $this->currentProductChannelId();
+        $key = \WishHistories::getGuardWishKey($product_channel_id);
+        $wish_histories = \WishHistories::findByRelationsForWish($key, $page, $per_page);
+        if (!$wish_histories) {
+            return $this->renderJSON(ERROR_CODE_FAIL, '没有更多的愿望哦，快来许愿吧！');
+        }
+        return $this->renderJSON(ERROR_CODE_SUCCESS, '', $wish_histories->toJson('wish_histories', 'toSimpleJson'));
+
+    }
+
+    function releaseWishAction()
+    {
+        $user = $this->currentUser();
+        $wish_text = $this->params('my_wish_text');
+        if (!$wish_text) {
+            return $this->renderJSON(ERROR_CODE_FAIL, '参数错误');
+        }
+        $amount = 5;
+        if ($user->diamond < $amount) {
+            return $this->renderJSON(ERROR_CODE_FAIL, '钻石不足');
+        }
+
+        $product_channel_id = $this->currentProductChannelId();
+        $opts = [
+            'user_id' => $user->id,
+            'amount' => $amount,
+            'wish_text' => $wish_text,
+            'product_channel_id' => $product_channel_id
+        ];
+        $result = \WishHistories::createWishHistories($opts);
+        if ($result) {
+            return $this->renderJSON(ERROR_CODE_SUCCESS, '发布成功！');
+        }
+    }
+
+    function myWishHistoriesAction()
+    {
+        $my_wish_histories = \WishHistories::find(['conditions' => 'user_id = ' . $this->currentUserId(), 'order' => 'id desc']);
+
+        $my_wish_datas = [];
+        foreach ($my_wish_histories as $my_wish_history) {
+            $my_wish_datas[] = $my_wish_history->wish_text;
+        }
+        return $this->renderJSON(ERROR_CODE_SUCCESS, '', ['my_wish_datas' => $my_wish_datas]);
+    }
+
+    function guardWishAction()
+    {
+        $user = $this->currentUser();
+        $wish_history_id = $this->params('wish_history_id');
+        $amount = 2;
+        if ($user->diamond < $amount) {
+            return $this->renderJSON(ERROR_CODE_FAIL, '钻石不足');
+        }
+        $opts = ['remark' => '守护愿望消耗' . $amount . '钻石', 'target_id' => $wish_history_id];
+        $result = \AccountHistories::changeBalance($user->id, ACCOUNT_TYPE_GUARD_WISH_EXPENSES, $amount, $opts);
+        if (!$result) {
+            return $this->renderJSON(ERROR_CODE_SUCCESS, '参数错误');
+        }
+
+        $product_channel_id = $this->currentProductChannelId();
+        $user_db = \Users::getUserDb();
+        $guard_wish_key = \WishHistories::getGuardWishKey($product_channel_id);
+        $guarded_number = $user_db->zincrby($guard_wish_key, 1, $wish_history_id);
+
+        return $this->renderJSON(ERROR_CODE_SUCCESS, '守护成功', ['guarded_number' => $guarded_number]);
+    }
+
+}
