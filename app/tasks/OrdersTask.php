9749ef5bad22c71a38e0da03e6a6b2edad0b714a
luolu <luolu@marmotad.cn>
2018-05-11 09:55:39 +0800

    pushRechargeFailed Task

--- app/tasks/OrdersTask.php
+++ app/tasks/OrdersTask.php
@@ -71,4 +71,50 @@ class OrdersTask extends \Phalcon\Cli\Task
 
         fclose($f);
     }
+
+
+    function pushRechargeFailedAction()
+    {
+        $time = strtotime('-1 hour', time());
+
+        $conditions = array(
+            'conditions' => 'status = :status: and created_at >= :created_at:',
+            'bind' => [
+                'status' => STATUS_OFF,
+                'created_at' => $time
+            ],
+            'columns'=>'user_id'
+        );
+        $order = Orders::find($conditions);
+
+        $count = array(); // 计数器
+        foreach ($order as $value) {
+            if (isset($count[$value->user_id])) {
+                $count[$value->user_id] += 1;
+            } else
+                $count[$value->user_id] = 1;
+        }
+
+        if (empty($count)) return ;
+        //$count[41792] = 4;
+
+        $content = '尊敬用户：您好！请问您是否在支付的时候遇到了问题？如有疑问请联系官方客服中心400-018-7755解决。';
+        $push_data = [
+            'title' => '系统充值通知',
+            'body' => $content
+        ];
+
+        // 次数大于2的user_id
+        foreach ($count as $item=>$value) {
+
+            if ($value >= 2) {
+                // 需要推送消息的
+                Chats::sendSystemMessage($item, CHAT_CONTENT_TYPE_TEXT, $content);
+
+                // 个推
+                $user = Users::findFirstById($item);
+                Pushers::push($user->getPushContext(), $user->getPushReceiverContext(), $push_data);
+            }
+        }
+    }
 }
