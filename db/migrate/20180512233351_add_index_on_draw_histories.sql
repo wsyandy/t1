ad8d5a30274f141ed78536d875114f2fcfcb37e0
kanggq <kanggq@365yf.com>
2018-05-12 23:40:14 +0800

    缓存

---
+++ db/migrate/20180512233351_add_index_on_draw_histories.sql
@@ -0,0 +1,3 @@
+create index user_id_on_draw_histories on draw_histories(user_id);
+create index created_at_on_draw_histories on draw_histories(created_at);
+create index number_on_draw_histories on draw_histories(number);
