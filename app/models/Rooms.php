ff3ee5c2fdbddde861be09884b724347fcb8f43a
梅兴昊 <apple@appledeiMac.local>
2018-05-14 22:56:47 +0800

    优化

--- app/models/Rooms.php
+++ app/models/Rooms.php
@@ -537,7 +537,7 @@ class Rooms extends BaseModel
         $hot_cache = Users::getHotWriteCache();
         $key = 'room_active_last_at_list';
         $time = time();
-        $room_ids = $hot_cache->zrevrangebyscore($key, $time, $time - $minutes * 60, ['limit' => [0, 200]]);
+        $room_ids = $hot_cache->zrevrangebyscore($key, $time, $time - $minutes * 60, ['limit' => [0, 100]]);
         $rooms = Rooms::findByIds($room_ids);
 
         return $rooms;
