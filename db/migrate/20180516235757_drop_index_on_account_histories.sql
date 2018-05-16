65d1ede1be776abc573f1cf8e76d12d37a9b859e
kanggq <kanggq@365yf.com>
2018-05-17 00:13:54 +0800

    优化account_histories

--- db/migrate/20180516235757_drop_index_on_account_histories.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE account_histories DROP COLUMN order_id;
-ALTER TABLE account_histories DROP COLUMN gift_order_id;
-ALTER TABLE account_histories DROP COLUMN hi_coin_history_id;
-ALTER TABLE account_histories DROP COLUMN country_id;
-
-DROP INDEX target_id_on_account_histories;
-DROP INDEX union_type_on_account_histories;
-
-create index fee_type_target_id_on_account_histories on account_histories(fee_type,target_id);
