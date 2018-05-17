8dfcd7ae2acffb2121297f22617aaff43145cb3e
梅兴昊 <apple@appledeiMac.local>
2018-05-17 23:28:53 +0800

    热门

--- app/models/Rooms.php
+++ app/models/Rooms.php
@@ -541,7 +541,7 @@ class Rooms extends BaseModel
 //        $room_ids = $hot_cache->zrevrangebyscore($key, $time, $time - $minutes * 60, ['limit' => [0, 200]]);
 //        info($room_ids);
 //
-        $start = time() - 1800;
+        $start = time() - 3600;
         $end = time();
         $room_ids = [];
 
