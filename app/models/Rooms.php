6445c0ba1ad0125a41d603d6eec040ff9443b523
梅兴昊 <apple@appledeiMac.local>
2018-05-20 00:04:35 +0800

    优化

--- app/models/Rooms.php
+++ app/models/Rooms.php
@@ -3124,19 +3124,30 @@ class Rooms extends BaseModel
         if ($hot_room_num > 0 && $hot_room_num <= 9) {
 
             $diff_num = 9 - $hot_room_num;
-            $room_ids = array_slice($room_ids, $diff_num, 1);
 
-            if (count($room_ids) > 1) {
-                $tmp_score = $room_ids[0];
+            if (count($room_ids) <= $diff_num) {
 
                 foreach ($hot_room_ids as $hot_room_id => $score) {
+                    $room_ids[$hot_room_id] = $score;
+                }
 
-                    if ($score > $tmp_score) {
-                        $room_ids[$hot_room_id] = $score;
-                    } else {
-                        $room_ids[$hot_room_id] = $tmp_score += 10;
+            } else {
+
+                $room_ids = array_slice($room_ids, $diff_num, 1);
+
+                if (count($room_ids) > 1) {
+                    $tmp_score = $room_ids[0];
+
+                    foreach ($hot_room_ids as $hot_room_id => $score) {
+
+                        if ($score > $tmp_score) {
+                            $room_ids[$hot_room_id] = $score;
+                        } else {
+                            $room_ids[$hot_room_id] = $tmp_score += 10;
+                        }
                     }
                 }
+
             }
         }
 
