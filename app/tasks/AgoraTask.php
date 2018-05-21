25890fa6a209a4543a10d28ff3f5340e75a3a582
kanggq <kanggq@365yf.com>
2018-05-21 14:31:06 +0800

    Agora

--- app/tasks/AgoraTask.php
+++ app/tasks/AgoraTask.php
@@ -6,23 +6,28 @@
  * Time: 下午6:05
  */
 
-class AgoraTask extends \Phalcon\Cli\Task {
+class AgoraTask extends \Phalcon\Cli\Task
+{
+
+    //b044e3fb33ab4f129ad0cd9dfd7e9d65
+    //9eeb8dc5543b4df7b1c832d841929f81
 
     //uid
-    function userAction($params){
+    function userAction($params)
+    {
 
         $params = [];
 
-        $uid =575899;
+        $uid = 1200587;
         $user = Users::findFirstByUid($uid);
         echoLine($user);
-        if(!$user){
+        if (!$user) {
             echoLine('error ', $params);
             return;
         }
 
         $room = $user->current_room;
-        if(!$room){
+        if (!$room) {
             echoLine('error room', $params);
             return;
         }
@@ -32,11 +37,33 @@ class AgoraTask extends \Phalcon\Cli\Task {
         $key = $product_channel->getChannelKey($channel_name, $user->id);
         $app_id = $product_channel->getImAppId();
 
+        //'Postman-Token' => '5590c096-35b2-4651-a840-7719f13dfcdf',
+        $headers = array(
+            'Cache-Control' => 'no-cache',
+            'Authorization' => 'Basic YjA0NGUzZmIzM2FiNGYxMjlhZDBjZDlkZmQ3ZTlkNjU6OWVlYjhkYzU1NDNiNGRmN2IxYzgzMmQ4NDE5MjlmODE='
+        );
         $url = "http://api.agora.io/dev/v1/channel/user/property/{$app_id}/{$user->id}/{$channel_name}";
-        $res = httpGet($url);
+        echoLine($url);
+        $res = httpGet($url, [], $headers);
 
         echoLine($res);
 
     }
+
+    // room_id
+    function userListAction($params)
+    {
+
+        $params = [1027722];
+        $room_id = $params[0];
+        $room = Rooms::findFirstById($room_id);
+        if (!$room) {
+            echoLine('error room', $params);
+            return;
+        }
+
+        $room->checkBroadcasters();
+    }
+
 
 }
