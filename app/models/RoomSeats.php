dd375a8a28087a71a9b786eaea5a91482d6a4577
梅兴昊 <apple@appledeiMac.local>
2018-05-22 11:32:40 +0800

    优化

--- app/models/RoomSeats.php
+++ app/models/RoomSeats.php
@@ -44,7 +44,8 @@ class RoomSeats extends BaseModel
         $data = [];
         $user = $this->user;
         if ($user) {
-            $data = ['sex' => $user->sex, 'avatar_small_url' => $user->avatar_small_url, 'nickname' => $user->nickname];
+            $data = ['sex' => $user->sex, 'avatar_small_url' => $user->avatar_small_url, 'nickname' => $user->nickname,
+                'avatar_100x100_url' => $user->avatar_100x100_url, 'avatar_60x60_url' => $user->avatar_60x60_url];
         }
 
         return $data;
