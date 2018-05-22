4084bc682208216d44302a95388dba1a98b2566f
梅兴昊 <apple@appledeiMac.local>
2018-05-23 00:08:16 +0800

    优化

--- app/tasks/MeiTask.php
+++ app/tasks/MeiTask.php
@@ -11,6 +11,27 @@ class MeiTask extends \Phalcon\Cli\Task
 
     function test60Action()
     {
+        $room = Rooms::findFirstById(16);
+
+        $body = ['action' => 'send_topic_msg', 'user_id' => 1, 'nickname' => '呵呵', 'sex' => 1,
+            'avatar_url' => '', 'avatar_small_url' => '', 'content' => '晚上好',
+            'channel_name' => $room->channel_name, 'content_type' => 'text'
+        ];
+
+        $user = Users::findFirstById(1001304);
+
+        $res = $room->pushToUser($user, $body);
+
+        echoLine($res);
+
+        $need_version_control = false;
+        if ($content_type == 'red_packet') {
+            $need_version_control = true;
+        }
+
+        $this->push($body, $need_version_control);
+
+
         $db = Users::getUserDb();
         $key = "room_manager_1";
         $db->hset($key, 2, json_encode(['time' => 12222, 'is_de' => false]));
