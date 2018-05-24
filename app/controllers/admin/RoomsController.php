76c7675607dce4ad9942fb15b81848e657d23336
梅兴昊 <apple@appledeiMac.local>
2018-05-24 14:10:12 +0800

    优化

--- app/controllers/admin/RoomsController.php
+++ app/controllers/admin/RoomsController.php
@@ -175,7 +175,7 @@ class RoomsController extends BaseController
             $svga_image_url = $this->params('svga_image_url');
             $total_value = $this->params('total_value');
             $current_value = $this->params('current_value');
-            $content_type = $this->params('content_type');
+            $content_type = $this->params('content_type', null);
             $title = $this->params('title');
 
             debug($action, $sender_id, $gift_id, $red_packet_num, $url, $content, $left_pk_user_id, $left_pk_user_score, $right_pk_user_id, $right_pk_user_score, $expire_at);
