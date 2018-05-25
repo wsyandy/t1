a72dbd8e87e2b0e8ea0e70214356ff8dee37654f
梅兴昊 <apple@appledeiMac.local>
2018-05-25 23:04:49 +0800

    优化

--- app/models/RoomStats.php
+++ app/models/RoomStats.php
@@ -349,7 +349,9 @@ trait RoomStats
 
                 //临时查询
                 $sender = Users::findFirstById($sender_id);
-                $this->pushTopTopicMessage($sender, "恭喜【{$sender->nickname}】在【{$this->name}】内，成功引爆火箭，快来抢礼物吧！");
+                $content = "恭喜【{$sender->nickname}】在【{$this->name}】内，成功引爆火箭，快来抢礼物吧！";
+                Rooms::delay()->asyncAllNoticePush($content,  ['type' => 'top_topic_message', 'hot' => 1]);
+                //$this->pushTopTopicMessage($sender, ['type' => 'top_topic_message', 'hot' => 1]);
                 unlock($lock);
 
                 return;
