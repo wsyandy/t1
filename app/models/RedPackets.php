1125b36d397447c910184cea3811a15d32c913fd
kanggq <kanggq@365yf.com>
2018-05-29 23:21:51 +0800

    红包

--- app/models/RedPackets.php
+++ app/models/RedPackets.php
@@ -159,8 +159,8 @@ class RedPackets extends BaseModel
         $geo_distance = intval($geo_distance);
         debug($user->id, $geo_distance);
         
-        $red_packet = self::findFirst(['conditions' => 'user_id=:user_id: and red_packet_type=:red_packet_type: and nearby_distance>=:nearby_distance: and (sex=2 or sex=:sex:)',
-            'bind' => ['user_id' => $user->id, 'red_packet_type' => RED_PACKET_TYPE_NEARBY, 'nearby_distance' => $geo_distance, 'sex' => $user->sex],
+        $red_packet = self::findFirst(['conditions' => 'user_id=:user_id: and red_packet_type=:red_packet_type: and nearby_distance>=:nearby_distance: and (sex=:sex1: or sex=:sex:)',
+            'bind' => ['user_id' => $user->id, 'red_packet_type' => RED_PACKET_TYPE_NEARBY, 'nearby_distance' => $geo_distance, 'sex1' => 2, 'sex' => $user->sex],
             'order' => 'id desc']);
 
         return $red_packet;
