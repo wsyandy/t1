7416894a14d94b3e3099e505b570cf6f64ba9916
梅兴昊 <apple@appledeiMac.local>
2018-05-31 14:07:25 +0800

    优化

--- app/models/Rooms.php
+++ app/models/Rooms.php
@@ -985,10 +985,7 @@ class Rooms extends BaseModel
 
     static function newSearchHotRooms($user, $page, $per_page)
     {
-        //$new_user_hot_rooms_list_key = Rooms::getNewUserHotRoomListKey(); //新用户房间
-        //$old_user_pay_hot_rooms_list_key = Rooms::getOldUserPayHotRoomListKey(); //充值老用户队列
-        //$old_user_no_pay_hot_rooms_list_key = Rooms::getOldUserNoPayHotRoomListKey(); //未充值老用户队列
-
+        $new_user_hot_rooms_list_key = Rooms::getNewUserHotRoomListKey(); //新用户房间
         $register_time = time() - $user->register_at;
         $time = 60 * 15;
 
@@ -999,21 +996,12 @@ class Rooms extends BaseModel
         if ($user->isShieldHotRoom()) {
 
             $hot_room_list_key = Rooms::getHotRoomListKey();
-
         } else {
 
             $hot_room_list_key = Rooms::getTotalRoomListKey(); //新的用户总的队列
-
-//            if ($register_time <= $time) {
-//                $hot_room_list_key = $new_user_hot_rooms_list_key;
-//            } else {
-//
-//                if ($user->pay_amount > 0) {
-//                    $hot_room_list_key = $old_user_pay_hot_rooms_list_key;
-//                } else {
-//                    $hot_room_list_key = $old_user_no_pay_hot_rooms_list_key;
-//                }
-//            }
+            if ($register_time <= $time) {
+                $hot_room_list_key = $new_user_hot_rooms_list_key;
+            }
         }
 
         $hot_cache = Users::getHotWriteCache();
