0a50ef4161daea67d2cc3832242423a5f61074ee
梅兴昊 <meixinghao@365yf.com>
2018-06-03 21:53:42 +0800

    权限

--- app/config/acl.php
+++ app/config/acl.php
@@ -49,7 +49,7 @@ return [
         'room_themes' => '*', 'broadcasts' => '*', 'albums' => '*', 'account_histories' => ['index'], 'gold_histories' => ['index'],
         'gift_orders' => ['detail'], 'user_gifts' => ['index'], 'voice_calls' => ['index'], 'union_histories' => ['basic'], 'hi_coin_histories' => ['basic', 'index'],
         'withdraw_histories' => ['basic'], 'activity_histories' => ['basic'], 'withdraw_accounts' => ['index'],
-        'payments' => ['index'], 'audios' => '*', 'audio_chapters' => '*', 'product_menus' => '*', 'room_tags' => '*', 'room_categories' => '*',
+        'audios' => '*', 'audio_chapters' => '*', 'product_menus' => '*', 'room_tags' => '*', 'room_categories' => '*',
         'gift_stats' => ['days'], 'draw_histories' => ['day_stat', 'hour_stat', 'index'], 'search_histories' => ['room']
     ],
     'product_operator_assistant' => ['dashboard' => '*', 'users' => ['index', 'detail', 'edit', 'update'], 'rooms' => '*',
