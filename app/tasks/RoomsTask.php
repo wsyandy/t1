0c1544cdb053674302bbb7c78513c45ba103f471
kanggq <kanggq@365yf.com>
2018-06-05 10:43:21 +0800

    房间检测

--- app/tasks/RoomsTask.php
+++ app/tasks/RoomsTask.php
@@ -13,7 +13,7 @@ class RoomsTask extends \Phalcon\Cli\Task
     {
 
         $cond = ['conditions' => 'status = :status: and last_at<:last_at:',
-            'bind' => ['status' => STATUS_ON, 'last_at' => time() - 3600 * 24],
+            'bind' => ['status' => STATUS_ON, 'last_at' => time() - 3600 * 12],
             'order' => 'id asc'
         ];
 
